7232808ce5b8d6ae54a1ae8df4a4dca0b9ca66f3
pinwormmy <ozkhan@naver.com>
2022-04-13 01:14:40 +0900

    아이템 리싸이클러 브랜드만 추가했는데...
    피곤해서 나머지 천천히 구현하자..으...

--- Memo2nd/app/src/main/java/com/eol/memo2nd/Memo.kt
+++ Memo2nd/app/src/main/java/com/eol/memo2nd/Memo.kt
@@ -1,5 +1,5 @@
 package com.eol.memo2nd
 
-data class Memo(var userId: Long?, var memoContent:String, var dateTime:Long ){}
+data class Memo(var userId: Long?, var brandMemo:String, var memoContent:String, var dateTime:Long ){}
 
 
