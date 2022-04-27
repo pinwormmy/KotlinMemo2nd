228da20ef573b8c23f11193e4c1eadf91cc724cc
pinwormmy <ozkhan@naver.com>
2022-04-28 00:56:12 +0900

    room 적용 중
    시계 추가 프래그먼트 수정하고 테스트해야함

--- Memo2nd/app/src/main/java/com/eol/memo2nd/AddWatchFragment.kt
+++ Memo2nd/app/src/main/java/com/eol/memo2nd/AddWatchFragment.kt
@@ -25,29 +25,32 @@ class AddWatchFragment() : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
-        var dbHelper: DBopenHelper = DBopenHelper(mainActivity,"Watch",null,2)
+        // var dbHelper: DBopenHelper = DBopenHelper(mainActivity,"Watch",null,2)
 
         binding = AddWatchBinding.inflate(inflater, container, false)
 
+        /*
         val adapter = WatchAdapter()
         adapter.listData.addAll(dbHelper.selectWatch())
         adapter.dbHelper = dbHelper
 
         binding.watchListRecycler.adapter = adapter
         binding.watchListRecycler.layoutManager = LinearLayoutManager(mainActivity)
-
+*/
 
         binding.saveButton.setOnClickListener {
             if (binding.writeBrand.text.toString().isNotEmpty() and binding.writeNameText.text.toString().isNotEmpty()
                 and binding.writeCaseSizeText.text.isNotEmpty() and binding.writeLugtoLugText.text.isNotEmpty()) {
                 val watch = Watch(null, binding.writeBrand.text.toString(), binding.writeNameText.text.toString(), binding.writeRefNumberText.text.toString(),
                     Integer.parseInt(binding.writeCaseSizeText.text.toString()),Integer.parseInt(binding.writeLugtoLugText.text.toString()), System.currentTimeMillis())
-                dbHelper.addWatch(watch)
+                // dbHelper.addWatch(watch)
             }
+            /*
             adapter.listData.clear()
             adapter.listData.addAll(dbHelper.selectWatch())
 
             adapter.notifyDataSetChanged()
+            */
             binding.writeBrand.setText("")
             binding.writeNameText.setText("")
             binding.writeRefNumberText.setText("")
