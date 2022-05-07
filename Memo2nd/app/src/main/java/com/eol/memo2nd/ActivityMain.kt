56a30efd3cdcfb4d5977613b0dfdff0123656545
pinwormmy <ozkhan@naver.com>
2022-05-08 00:40:51 +0900

    손볼게 많지만, 일단 구글 로그인 적용먼저
    코드 수정은 아직 안들어감

--- Memo2nd/app/src/main/java/com/eol/memo2nd/ActivityMain.kt
+++ Memo2nd/app/src/main/java/com/eol/memo2nd/ActivityMain.kt
@@ -1,16 +1,15 @@
 package com.eol.memo2nd
 
-// 품번입력시 디비에는 쩜이랑 특문 빼고 등록하게끔(아니면 알아서 제외시켜 등록하는쪽으로)
+// 이전에 구글 로그인 적용해보긔
+
+// 품번입력시 디비에는 쩜이랑 특문 빼고 등록하게끔(아니면 알아서 제외시켜 등록하는쪽으로) <- 이거 어떻게 처리할거야
 // 사이즈 소수점 입력하게 자료타입 설정. nullable 로해서 thickness 값도 등록하긔
 
-// 그래서, DB개편이 필요한데, room 새 버전할지, 아니면 firebase 쓸지
-// 위키시스템과 DB연계 상황을 보고 결정
-
-// 그래서 위키시스템 어떻게 구성할지부터 결정해야함.
+// 그래서, DB개편이 필요한데, room 새 버전할지, 아니면 firebase 쓸지, 위키시스템과 DB연계 상황을 보고 결정
+// 그래서 위키시스템 어떻게 구성할지부터 결정해야함. 정정요청시스템에 따로 db구조 바꿀 필요없으면 그대로 간다
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import com.eol.memo2nd.databinding.ActivityMainBinding
 import com.google.firebase.auth.FirebaseAuth
 
