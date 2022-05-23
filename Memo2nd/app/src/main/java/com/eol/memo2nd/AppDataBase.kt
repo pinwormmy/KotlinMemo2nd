a6904f9ed362e19af16806debf99bad971514bf8
pinwormmy <ozkhan@naver.com>
2022-05-24 00:50:28 +0900

    딴거하느라 거의 작업안댐2
    수동 마이그레이션 진행예정

--- Memo2nd/app/src/main/java/com/eol/memo2nd/AppDataBase.kt
+++ Memo2nd/app/src/main/java/com/eol/memo2nd/AppDataBase.kt
@@ -7,6 +7,7 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 // 수동 마이그레이션 추가해서 다시 수정
 // 마이그레이션 안되면 db새로 생성 (버전 업데이트때문에 안될 수 있어서)
+// 별개로 마이그레이션 방법 숙지는 해야함
 
 @Database(
     version = 2, // 수정해야함
