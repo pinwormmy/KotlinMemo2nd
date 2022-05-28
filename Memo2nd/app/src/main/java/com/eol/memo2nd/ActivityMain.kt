64b5f3fe5988707dc5e687d4cf3a2dc03508849b
pinwormmy <ozkhan@naver.com>
2022-05-29 00:47:19 +0900

    시험도 쳤으니, 이제 빡코딩하자
    수동 마이그레이션 진행예정

--- Memo2nd/app/src/main/java/com/eol/memo2nd/ActivityMain.kt
+++ Memo2nd/app/src/main/java/com/eol/memo2nd/ActivityMain.kt
@@ -1,8 +1,8 @@
 package com.eol.memo2nd
 
 // db 수정 중(마이그레이션해야하는데 다른 일때미 자꾸 지연 중...)
-
 // 품번입력시 디비에는 쩜이랑 특문 빼고 등록하게끔 XML DIGIT 옵션 넣었는데 확인해보기
+
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -42,7 +42,6 @@ class MainActivity : AppCompatActivity() {
             5 -> ft.replace(R.id.mainFrame, LoginFragment()).commit()
         }
     }
-
 }
 
 
